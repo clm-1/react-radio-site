0cfc81559192a61df67a8d20ba7ce87cd168c2ec
Chris <to.chris.lindstrom@gmail.com>
2021-04-21 12:14:35 +0200

    Fetch channels and categories to Context

--- src/contexts/RadioDataContext.jsx
+++ src/contexts/RadioDataContext.jsx
@@ -3,9 +3,31 @@ import { createContext, useState, useEffect } from 'react';
 export const RadioDataContext = createContext();
 
 const RadioDataProvider = (props) => {
+  const [channels, setChannels] = useState(null);
+  const [categories, setCategories] = useState(null);
+
+  const getAllChannels = async () => {
+    let channels = await fetch('/api/v1/channels');
+    channels = await channels.json();
+    console.log(channels.channels);
+    setChannels(channels);
+  }
+
+  const getAllCatgories = async () => {
+    let categories = await fetch('/api/v1/categories');
+    categories = await categories.json();
+    console.log(categories.programcategories);
+    setCategories(categories);
+  }
+
+  useEffect(() => {
+    getAllChannels();
+    getAllCatgories();
+  }, []);
   
   const values = {
-
+    channels,
+    categories,
   };
 
   return (
