5e62c7b321fc31a509b81c48243f32c1d5021d72
Chris <to.chris.lindstrom@gmail.com>
2021-04-21 17:37:02 +0200

    Get user by id

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -6,11 +6,19 @@ const db = new sqlite3.Database(path.join(__dirname, '../radioSiteDb.db'));
 const getAllUsers = (req, res) => {
   let query = `SELECT * FROM users`;
   db.all(query, (err, users) => {
-    console.log('getting users', users)
     res.send(users);
+  })
+}
+
+const getUserById = (req, res) => {
+  let query = `SELECT * FROM users WHERE userId = $userId`;
+  let params = { $userId: req.params.userId };
+  db.get(query, params, (err, user) => {
+    res.json(user);
   })
 }
 
 module.exports = {
   getAllUsers,
+  getUserById,
 }
